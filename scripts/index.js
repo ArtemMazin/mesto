16a93693114c995b2bb19d802d11be84c92bc299
ArtemMazin <minima1s@yandex.ru>
2022-12-17 00:45:57 +0300

    feat: добавить редактирование профиля

--- scripts/index.js
+++ scripts/index.js
@@ -1,8 +1,8 @@
 'use strict'
 
-const openModalBtn = document.querySelector('.profile__edit-button'),
-      modal = document.querySelector('.popup'),
-      closeModalBtn = modal.querySelector('.popup__close-btn');
+const modal = document.querySelector('.popup'),
+  openModalBtn = document.querySelector('.profile__edit-button'),
+  closeModalBtn = modal.querySelector('.popup__close-btn');
 
 function openModal() {
   modal.classList.add('popup_opened');
@@ -13,3 +13,22 @@ function closeModal() {
 
 openModalBtn.addEventListener('click', openModal);
 closeModalBtn.addEventListener('click', closeModal);
+
+// -----------------------------------------------------------------------------------------
+
+const formElement = document.querySelector('.popup'),
+  nameInput = formElement.querySelector('.popup__name'),
+  jobInput = formElement.querySelector('.popup__job'),
+  nameProfile = document.querySelector('.profile__name'),
+  jobProfile = document.querySelector('.profile__job');
+
+function handleFormSubmit(evt) {
+  evt.preventDefault();
+  nameProfile.textContent = nameInput.value;
+  jobProfile.textContent = jobInput.value;
+}
+
+formElement.addEventListener('submit', handleFormSubmit);
+
+// ------------------------------------------------------------------------------
+
