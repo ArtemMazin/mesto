1acf251295ca8169bac3938952b33b486a281a9f
ArtemMazin <minima1s@yandex.ru>
2022-12-17 22:31:26 +0300

    fix: начать исправлять ошибки

--- scripts/index.js
+++ scripts/index.js
@@ -2,37 +2,25 @@
 
 // ----------------------------Модальное окно---------------------------------------------
 
-const body = document.querySelector('.body'),
-  modal = body.querySelector('.popup'),
-  openModalBtn = body.querySelector('.profile__edit-button'),
+const modal = document.querySelector('.popup'),
+  openModalBtn = document.querySelector('.profile__edit-button'),
   closeModalBtn = modal.querySelector('.popup__close-btn'),
   nameInput = modal.querySelector('.popup__name'),
   jobInput = modal.querySelector('.popup__job'),
-  nameProfile = body.querySelector('.profile__name'),
-  jobProfile = body.querySelector('.profile__job');
-
+  nameProfile = document.querySelector('.profile__name'),
+  jobProfile = document.querySelector('.profile__job');
 
 function openModal() {
   modal.classList.add('popup_opened');
-  body.style.overflow = 'hidden';
+  nameInput.value = nameProfile.textContent;
+  jobInput.value = jobProfile.textContent;
 }
 function closeModal() {
   modal.classList.remove('popup_opened');
-  body.style.overflow = '';
 }
 
 openModalBtn.addEventListener('click', openModal);
-
-modal.addEventListener('keydown', (e) => {
-  if (e.key === 'Escape') {
-    closeModal();
-  }
-});
-modal.addEventListener('click', (e) => {
-  if (e.target === modal || e.target === closeModalBtn) {
-    closeModal();
-  }
-})
+closeModalBtn.addEventListener('click', closeModal);
 
 function handleFormSubmit(e) {
   e.preventDefault();
