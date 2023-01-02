0ffeb5163004a21c270c27a36ce3da1455771212
ArtemMazin <minima1s@yandex.ru>
2023-01-02 18:28:26 +0300

    feat: добавить карточки из массива

--- scripts/index.js
+++ scripts/index.js
@@ -33,3 +33,48 @@ modal.addEventListener('submit', handleFormSubmit);
 
 // ------------------------------------------------------------------------------
 
+const cardTemplate = document.querySelector('#card').content,
+      cardItems = document.querySelector('.cards__items'),
+      initialCards = [
+        {
+          name: 'Архыз',
+          link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/arkhyz.jpg'
+        },
+        {
+          name: 'Челябинская область',
+          link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/chelyabinsk-oblast.jpg'
+        },
+        {
+          name: 'Иваново',
+          link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/ivanovo.jpg'
+        },
+        {
+          name: 'Камчатка',
+          link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kamchatka.jpg'
+        },
+        {
+          name: 'Холмогорский район',
+          link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kholmogorsky-rayon.jpg'
+        },
+        {
+          name: 'Байкал',
+          link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/baikal.jpg'
+        }
+      ];
+
+let newCard = {
+  name: 'Холмогорский район',
+  link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kholmogorsky-rayon.jpg'
+}
+initialCards.unshift(newCard);
+
+initialCards.forEach((card) => {
+  const cardElement = cardTemplate.querySelector('.cards__item').cloneNode(true);
+  cardElement.querySelector('.cards__image').src = card.link;
+  cardElement.querySelector('.cards__text').textContent = card.name;
+  
+  cardItems.append(cardElement); 
+})
+
+
+
